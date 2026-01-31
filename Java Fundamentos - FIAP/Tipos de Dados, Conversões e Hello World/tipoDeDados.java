4dcb69ff75cb13628d78e51c81a81dbe2c8aee7c
Gabriel Gonçalves Santana <gabriel.goncalves.santanna@gmail.com>
2026-01-31 14:51:07 -0300

    Add files via upload

---
+++ Java Fundamentos - FIAP/Tipos de Dados, Conversões e Hello World/tipoDeDados.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class tipoDeDados {
+
+	public static void main(String[] args) {
+		
+		int idade = 18;
+		double altura = 1.67;
+		char sexo = 'M';
+		boolean temCNH = false;
+		
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println(sexo);
+		System.out.println(temCNH);
+
+	}
+
+}
